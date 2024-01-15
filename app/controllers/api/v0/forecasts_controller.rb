d927e3c3896822ae76cb6751bf594d854b119972
Anthea Yur <acyur6@gmail.com>
2024-01-15 01:01:37 -0600

    Start forecast controller with converting city and state to coordinates

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -1,7 +1,15 @@
 class Api::V0::ForecastsController < ApplicationController
 
   def show
+    conn = Faraday.new(url: "https://www.mapquestapi.com") do |faraday|
+      faraday.params["key"] = Rails.application.credentials.mapquest[:key]
+    end
+    response = conn.get("/geocoding/v1/address?location=#{params[:location]}")
+    data = JSON.parse(response.body, symbolize_names: true)[:results].first[:locations].first[:latLng]
 
+    coordinates = data.map do |coords|
+      Coordinates.new(coords)
+    end
   end
-  
+
 end
