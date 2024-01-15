149efe5544dfd95b2c5d6caead87aaeb6fbb5146
Anthea Yur <acyur6@gmail.com>
2024-01-15 03:17:16 -0600

    Add testing for daily_weather attribute

--- spec/api/v0/requests/forecast_request_spec.rb
+++ spec/api/v0/requests/forecast_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Forecast", type: :request do
     get "/api/v0/forecast?location=minneapolis,mn"
     expect(response).to be_successful
     @mpls_forecast = JSON.parse(response.body, symbolize_names: true)[:data]
+    @attributes = @mpls_forecast[:attributes]
   end
 
   describe "get forecast data based on the city and state inputted" do
@@ -14,13 +15,17 @@ RSpec.describe "Forecast", type: :request do
       expect(@mpls_forecast[:type]).to eq("forecast")
       expect(@mpls_forecast).to have_key(:attributes)
     end
+    
+    it "has three attributes of daily, hourly, and current weather" do
+      expect(@attributes).to have_key(:daily_weather)
+      expect(@attributes).to have_key(:hourly_weather)
+    end
 
     it 'contains current_weather as an attribute that holds the last_updated etc info' do
-      expect(@mpls_forecast[:attributes]).to have_key(:current_weather)
-      expect(@mpls_forecast[:attributes]).to be_a(Hash)
-
       current = @mpls_forecast[:attributes][:current_weather]
       
+      expect(current).to be_a(Hash)
+
       expect(current).to have_key(:last_updated)
       expect(current[:last_updated]).to be_a(String)
 
@@ -44,11 +49,36 @@ RSpec.describe "Forecast", type: :request do
 
       expect(current).to have_key(:icon)
       expect(current[:icon]).to be_a(String)
-
     end
 
-    xit 'contains daily_weather as an attribute that is an array of the next 5 days of data' do
 
+    it 'contains daily_weather as an attribute that is an array of the next 5 days of data' do
+      daily = @mpls_forecast[:attributes][:daily_weather]
+      expect(daily).to be_an(Array)
+      expect(daily.length).to eq(5)
+
+      daily.each do |day|
+        expect(day).to have_key(:date)
+        expect(day[:date]).to be_a(String)
+
+        expect(day).to have_key(:sunrise)
+        expect(day[:sunrise]).to be_a(String)
+
+        expect(day).to have_key(:sunset)
+        expect(day[:sunset]).to be_a(String)
+
+        expect(day).to have_key(:max_temp)
+        expect(day[:max_temp]).to be_a(Float)
+
+        expect(day).to have_key(:min_temp)
+        expect(day[:min_temp]).to be_a(Float)
+        
+        expect(day).to have_key(:condition)
+        expect(day[:condition]).to be_a(String)
+
+        expect(day).to have_key(:icon)
+        expect(day[:icon]).to be_a(String)
+      end
     end
 
     xit 'contains hourly_weather as an attribute that is an array of all 24 hours of hourly data for the current day' do
