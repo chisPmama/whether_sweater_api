e61d6f06df2bb9fb3db1836d7e3849c207c06d43
Anthea Yur <acyur6@gmail.com>
2024-01-15 03:58:58 -0600

    reformat

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -1,29 +1,26 @@
 class Api::V0::ForecastsController < ApplicationController
   def show
     coordinates = find_coordinates
-    @forecast = create_forecast(coordinates)
+    forecast = create_forecast(coordinates)
+    render json: ForecastSerializer.new(forecast)
   end
 
-  private 
+  private
   def find_coordinates
     location = params[:location]
-    @facade = CoordinateFacade.new
-    @coordinates = @facade.get_coordinates(location)
+    CoordinateFacade.new.get_coordinates(location)
   end
 
   def current_weather(coordinates)
-    @facade = WeatherFacade.new
-    @current = @facade.get_current_weather(coordinates)
+    WeatherFacade.new.get_current_weather(coordinates)
   end
 
   def hourly_weather(coordinates)
-    @facade = WeatherFacade.new
-    @hourly = @facade.get_hourly_weather(coordinates)
+    WeatherFacade.new.get_hourly_weather(coordinates)
   end
 
   def daily_weather(coordinates)
-    @facade = WeatherFacade.new
-    @daily = @facade.get_daily_weather(coordinates)
+    WeatherFacade.new.get_daily_weather(coordinates)
   end
 
   def create_forecast(coordinates)
@@ -31,8 +28,6 @@ class Api::V0::ForecastsController < ApplicationController
     daily = daily_weather(coordinates)
     hourly = hourly_weather(coordinates)
 
-    forecast = Forecast.new(current, daily, hourly)
-    render json: 
-    ForecastSerializer.new(forecast)
+    Forecast.new(current, daily, hourly)
   end
 end
