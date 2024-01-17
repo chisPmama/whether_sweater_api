f9538d54ec63cd9d80202a51171453a635adce91
Anthea Yur <acyur6@gmail.com>
2024-01-17 01:52:01 -0600

    Add calculation for destination eta

--- app/controllers/api/v0/road_trips_controller.rb
+++ app/controllers/api/v0/road_trips_controller.rb
@@ -20,8 +20,11 @@ class Api::V0::RoadTripsController < ApplicationController
     start_city = pretty_city(params[:origin])
     end_city = pretty_city(params[:destination])
     travel_time = data[:formattedTime]
+    calc_trav_sec = data[:time].to_f
 
-    
+    ## impossible route possibility
+    datetime = calculate_eta(calc_trav_sec)
+    temperature = find_weather_at_eta(travel_time)
     binding.pry
     # coordinates = find_coordinates
     # forecast = create_forecast(coordinates)
@@ -43,5 +46,15 @@ class Api::V0::RoadTripsController < ApplicationController
   def authenticate_user
     user = User.find_by(api_key: params[:api_key])
     return error_response("Invalid API key.", 422) unless user.id == session[:user_id]
+  end
+
+  def calculate_eta(travel_sec)
+    current_time = Time.now
+    eta = current_time + travel_sec
+    pretty_eta = eta.strftime("%Y-%m-%d %H:%M")
+  end
+
+  def find_weather_at_eta(travel_time)
+
   end
 end
