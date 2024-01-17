f4717ea49603c6fbd9781ed391797c438a15c887
Anthea Yur <acyur6@gmail.com>
2024-01-16 18:20:30 -0600

    Add error handling for creating a user

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,16 +1,25 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    if params[:password] == params[:password_confirmation]
+    if !params[:email]
+      error_response("E-mail cannot be blank.", 422)
+    elsif !params[:password] || !params[:password_confirmation]
+      error_response("Missing password entry.", 422)
+    elsif params[:password] != params[:password_confirmation]
+      error_response("Password and password confirmation do not match.", 422)
+    else
       user = User.create!(user_params)
-      render json: UsersSerializer.new(user), status: 201
-    else params[:password] != params[:password_confirmation]
-      # render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: UsersSerializer.new(user), status: :created
     end
   end
 
   private
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def error_response(message, status)
+    render json: ErrorSerializer.new(ErrorMessage.new(message, status))
+    .serialize_json, status: :unprocessable_entity
   end
   
 end
--- spec/api/v0/requests/user_registration_request_spec.rb
+++ spec/api/v0/requests/user_registration_request_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Create a New User", type: :request do
-  before :each do
-    user_info = {
-                "email": "chisPwants2code@goodgirl.com",
-                "password": "dogeatworld",
-                "password_confirmation": "dogeatworld"
-              }
-
-    post "/api/v0/users", params: user_info
-
-    expect(response).to be_successful
-    expect(response.status).to eq(201)
-
-    @result = JSON.parse(response.body, symbolize_names: true)[:data]
-    @attributes = @result[:attributes]
-  end
-
+  
   describe "JSON User Registration Response" do
+    before :each do
+      user_info = {
+                  "email": "chisPwants2code@goodgirl.com",
+                  "password": "dogeatworld",
+                  "password_confirmation": "dogeatworld"
+                }
+  
+      post "/api/v0/users", params: user_info
+  
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+  
+      @result = JSON.parse(response.body, symbolize_names: true)[:data]
+      @attributes = @result[:attributes]
+    end
+
     it 'create a response that holds a type, id, and attribute following proper JSON format' do
       expect(@result).to be_a(Hash)
       expect(@result).to have_key(:id)
       expect(@result).to have_key(:type)
-      expect(@result[:type]).to eq("user")
+      expect(@result[:type]).to eq("users")
       expect(@result).to have_key(:attributes)
     end
 
@@ -38,6 +39,32 @@ RSpec.describe "Create a New User", type: :request do
 
     it 'contains the user e-mail that was inputted' do
       expect(@attributes[:email]).to eq("chisPwants2code@goodgirl.com")
+    end
+  end
+
+  describe 'User cannot create a password if not matching' do
+    before :each do
+      user_info = {
+                  "email": "chisPwants2code@goodgirl.com",
+                  "password": "dogeatworld",
+                  "password_confirmation": "dogwantsbone"
+                }
+  
+      post "/api/v0/users", params: user_info
+  
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+    end
+
+    it 'sends a JSON with the status of a 404 as well as an error message' do
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("422")
+      expect(data[:errors].first[:detail]).to eq("Password and password confirmation do not match.")
     end
   end
 
